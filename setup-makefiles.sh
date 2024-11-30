b12129740416ed6be3cf07fd6afedc41cb8c4c7d
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-30 17:51:40 -0500

    garnet: fix vendor.qti.hardware.wifidisplaysession@1.0 ELF checks
    Change-Id: I6afcbb57df52722cb251ce2aaa81a48ebbcb03dd
    Signed-off-by: Ramadhani <ramadhanifals@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -47,6 +47,7 @@ function lib_to_package_fixup_vendor_variants() {
         vendor.qti.hardware.qccsyshal@1.0 | \
         vendor.qti.hardware.qccsyshal@1.1 | \
         vendor.qti.hardware.qccvndhal@1.0 | \
+        vendor.qti.hardware.wifidisplaysession@1.0 | \
         vendor.qti.imsrtpservice@3.0)
             echo "$1-vendor"
             ;;
