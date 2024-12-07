809f4ab8721dcf3acfaef2d8d2295caee1ec86e5
Nick Reuter <nreuter85@gmail.com>
2024-12-07 15:59:17 -0600

    garnet: Bump libwfdservice to android.media.audio.common.types-V4
    Change-Id: I81060d8c73430a9598c676e18dc04e31a2f31c0c
    Signed-off-by: Ramadhani <ramadhanifals@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -72,7 +72,7 @@ function blob_fixup() {
             grep -q "libinput_shim.so" "${2}" || "${PATCHELF}" --add-needed "libinput_shim.so" "${2}"
             ;;
         system_ext/lib64/libwfdservice.so)
-            "${PATCHELF}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V3-cpp.so" "${2}"
+            "${PATCHELF}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V4-cpp.so" "${2}"
             ;;
         vendor/bin/hw/android.hardware.security.keymint-service-qti|vendor/lib64/libqtikeymint.so)
             "${PATCHELF_0_17_2}" --replace-needed "android.hardware.security.keymint-V1-ndk_platform.so" "android.hardware.security.keymint-V1-ndk.so" "${2}"
