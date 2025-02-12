0b6a869dbea6fa7c1b9828bfb48d13879054f086
Ramadhani <ramadhanifals@gmail.com>
2025-02-12 09:54:01 +0700

    [TMP] garnet: Clone audio agm and pal * I'm working on fixing elf and building audio straight from source, this will be useless until I get elf fixed in garnet
    Change-Id: Ic654585c292ff430b96754e6cb856caa4e3ff4b0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,5 +16,12 @@ git clone https://codeberg.org/ramaadni/vendor_xiaomi_miuicamera-garnet.git vend
 
 # Hw/ximi
 echo "Clone Hardware Xiaomi .."
+rm -rf hardware/oplus
 rm -rf hardware/xiaomi
 git clone https://github.com/los-garnet/android_hardware_xiaomi -b flare hardware/xiaomi --depth=1
+
+# Audio AGM and PAL
+echo "Clone Audio AGM and PAL .."
+rm -rf hardware/qcom-caf/sm8450/audio/agm && rm -rf hardware/qcom-caf/sm8450/audio/pal
+git clone https://github.com/los-garnet/android_vendor_qcom_opensource_agm.git hardware/qcom-caf/sm8450/audio/agm
+git clone https://github.com/los-garnet/android_vendor_qcom_opensource_arpal-lx.git hardware/qcom-caf/sm8450/audio/pal
