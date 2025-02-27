garnet-stuff/device_xiaomi_garnet

garnet: Rename prebuilt libgrpc++_unsecure.so for QCC
Change-Id: I7a0157e50c0eb0d1d84b4584f55e374f4b099483
Signed-off-by: Ramadhani <ramadhanifals@gmail.com>

extract-files.sh
@@ -100,9 +100,15 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
+        vendor/bin/qcc-trd)
+            "${PATCHELF}" --replace-needed "libgrpc++_unsecure.so" "libgrpc++_unsecure_prebuilt.so" "${2}"
+            ;;
         vendor/lib64/hw/audio.primary.parrot.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
+        vendor/lib64/libgrpc++_unsecure_prebuilt.so)
+            "${PATCHELF}" --set-soname "libgrpc++_unsecure_prebuilt.so" "${2}"
             ;;
         vendor/lib/libstagefright_soft_ac4dec.so | vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib/libstagefrightdolby.so | vendor/lib64/libdlbdsservice.so | vendor/lib64/libstagefright_soft_ac4dec.so | vendor/lib64/libstagefright_soft_ddpdec.so | vendor/lib64/libstagefrightdolby.so)
             [ "$2" = "" ] && return 0

los-garnet/android_device_xiaomi_garnet

Change-Id: Ia468e586385a2ce7dd0cfeddded2492cb4d9b776

extract-files.sh
@@ -73,6 +73,10 @@ function blob_fixup() {
             "${PATCHELF_0_17_2}" --replace-needed "android.hardware.security.sharedsecret-V1-ndk_platform.so" "android.hardware.security.sharedsecret-V1-ndk.so" "${2}"
             grep -q "android.hardware.security.rkp-V1-ndk.so" "${2}" || "${PATCHELF_0_17_2}" --add-needed "android.hardware.security.rkp-V1-ndk.so" "${2}"
             ;;
+        vendor/bin/qcc-trd)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF_0_17_2}" --replace-needed "libgrpc++_unsecure.so" "libgrpc++_unsecure_prebuilt.so" "${2}"
+            ;;
         vendor/etc/camera/pureView_parameter.xml)
             [ "$2" = "" ] && return 0
             sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
@@ -96,6 +100,10 @@ function blob_fixup() {
         vendor/etc/vintf/manifest/c2_manifest_vendor.xml)
             [ "$2" = "" ] && return 0
             sed -ni '/dolby/!p' "${2}"
+            ;;
+        vendor/lib64/libgrpc++_unsecure_prebuilt.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF_0_17_2}" --set-soname "libgrpc++_unsecure_prebuilt.so" "${2}"
             ;;
         vendor/lib64/vendor.libdpmframework.so)
             [ "$2" = "" ] && return 0